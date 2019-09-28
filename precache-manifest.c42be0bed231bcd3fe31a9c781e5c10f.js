848b59d9856f950d03b9065b9555423e789ae319
tyakym <Taras.Yakymiuk@nike.com>
2019-09-28 22:54:43 +0300

    Updates

---
+++ precache-manifest.c42be0bed231bcd3fe31a9c781e5c10f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d944c001efbe0e1b79b20a71cc1fb3f",
+    "url": "/anon-feedback/index.html"
+  },
+  {
+    "revision": "69c3a0e8845e990e21a6",
+    "url": "/anon-feedback/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "72432811df1eaee9b08a",
+    "url": "/anon-feedback/static/js/2.9dba4473.chunk.js"
+  },
+  {
+    "revision": "69c3a0e8845e990e21a6",
+    "url": "/anon-feedback/static/js/main.2cb104d3.chunk.js"
+  },
+  {
+    "revision": "4efda9a66a165c47b7bc",
+    "url": "/anon-feedback/static/js/runtime-main.f128e81a.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/anon-feedback/static/media/logo.25bf045c.svg"
+  }
+]);

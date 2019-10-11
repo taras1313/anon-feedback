88397f2c9a8a85439c08786d72df40e025cc99a4
smolch <stanislav.molchanov@liveramp.com>
2019-10-11 12:17:22 +0300

    [create-header-component] initial

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import './App.css';
 import Counter from './components/Counter/Counter';
 import { connect } from 'react-redux';
 import { increaseCounter, decreaseCounter } from './actions/actions';
+import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
 
 function App(props) {
     return (
       <div className="App">
-        <Counter counter={props.counter} increaseCounter={props.actions.increaseCounter} decreaseCounter={props.actions.decreaseCounter} />
+        <Header />
+        <Navbar />
       </div>
     );
 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,96 @@
+import React from 'react';
+
+import logo from './logo.svg';
+
+import { fade, makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import InputBase from '@material-ui/core/InputBase';
+import SearchIcon from '@material-ui/icons/Search';
+
+import styles from './Header.module.scss';
+
+const useStyles = makeStyles(theme => ({
+  header: {
+    backgroundColor: '#4f5a6e',
+    display: 'flex',
+    flexDirection: 'row',
+    height: '70px'
+  },
+  search: {
+    position: 'relative',
+    border: '1px solid grey',
+    borderRadius: '5px',
+    backgroundColor: 'transparent',
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.1),
+    },
+    alignSelf: 'center',
+    height: '34px',
+    marginRight: '20px'
+  },
+  inputInput: {
+    padding: '7px',
+    paddingLeft: '40px',
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    height: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: 200,
+    },
+  },
+  searchIcon: {
+    width: '45px',
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'white'
+  },
+  createThreadButton: {
+    backgroundColor: '#65c178',
+    color: 'white',
+    '&:hover': {
+      backgroundColor: '#159776'
+    },
+    width: '200px'
+  }
+}));
+
+export default function ButtonAppBar(props) {
+  const classes = useStyles();
+
+  return (
+    <AppBar position="static" className={classes.header}>
+      <div className={styles.logoWrapper}>
+        <img className={styles.logo} src={logo} alt='logo'/>
+      </div>
+      <div className={styles.verticalDivider} />
+      
+      <div className={styles.searchWrapper}>
+        <div className={classes.search}>
+          <div className={classes.searchIcon}>
+            <SearchIcon />
+          </div>
+          <InputBase
+            placeholder="Search…"
+            classes={{
+              root: classes.inputRoot,
+              input: classes.inputInput,
+            }}
+            inputProps={{ 'aria-label': 'search' }}
+          />
+        </div>
+      </div>
+      <div className={styles.verticalDivider} />
+      <div className={styles.createThreadWrapper}> 
+        <Button variant='contained' className={classes.createThreadButton}> create thread </Button> 
+      </div>
+
+    </AppBar>
+  );
+}

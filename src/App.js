946eb2c1fec833aa387012753844003a125e82b7
tyakym <Taras.Yakymiuk@nike.com>
2019-11-05 13:44:51 +0200

    created button for auth

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
 import Layout from './components/Layout/Layout';
 
@@ -9,12 +10,50 @@ import './App.css';
 
 
 import './App.css';
-function App(props) {
+
+class App extends Component {
+  state = {
+    loggedIn: false
+  };
+
+  componentDidMount() {
+    window.gapi.load('auth2', () => {
+      window.gapi.auth2.init({
+        client_id: '232822822071-1q970aukj04j6cv7p5buinsqs5vn0ks2'
+      }).then(console.log)
+    })
+  }
+
+  loginHandler = () => {
+    const GoogleAuth = window.gapi.auth2.getAuthInstance();
+
+    // GoogleAuth.signIn().then(console.log);
+    GoogleAuth.signIn().then((data) => {
+      this.setState({ loggedIn: true });
+      console.log(data.getAuthResponse().id_token);
+      axios.post('http://localhost:5000/', {token: data.getAuthResponse().id_token})
+
+    });
+  };
+
+  logOutHandler = () => {
+    const GoogleAuth = window.gapi.auth2.getAuthInstance();
+
+    // GoogleAuth.signIn().then(console.log);
+    GoogleAuth.signOut().then(() => this.setState({ loggedIn: false }));
+  };
+
+  render() {
+    const { loggedIn } = this.state;
+
     return (
       <div className="App">
+        {!loggedIn && <button onClick={this.loginHandler}>Kek in</button>}
+        {loggedIn && <button onClick={this.logOutHandler}>Kek Out</button>}
         <Layout />
       </div>
     );
+  }
 
 }
 
@@ -31,6 +70,6 @@ const mapDispatchToProps = dispatch => {
       decreaseCounter: () => dispatch(decreaseCounter())
     }
   }
-}
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/services/constants.js
+++ src/services/constants.js
@@ -1,4 +1,4 @@
-export const baseUrlDev = 'http://localhost:8080';
+export const baseUrlDev = 'http://localhost:5000';
 export const baseUrlProd = 'add here url for prod';
 export const pathList = {
   user: {
